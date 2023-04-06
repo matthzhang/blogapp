6d89c8320b168b006c574b38ba2639b4d2e5c1ac
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-04-06 16:16:09 -0500

    create single blog page and write page, made menu component

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="navBar">
+            <div className="container">
+                <div className="logo">
+                    <img src={Logo} alt="" />
+                </div>
+                <div className="links">
+                    <Link className="link" to="/?cat=art">
+                        <h6>Art</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=science">
+                        <h6>Science</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=technology">
+                        <h6>Technology</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=food">
+                        <h6>Food</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=design">
+                        <h6>Design</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=cinema">
+                        <h6>Cinema</h6>
+                    </Link>
+                    <span>John</span>
+                    <span>Logout</span>
+                    <span className="write">
+                        <Link className="link" to="/write">Write</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import Delete from "../img/delete.png"
+import Edit from "../img/edit.png"
+import { Link } from "react-router-dom";
+import Menu from "../componenets/Menu";
 
 const Single = () => {
     return (
-        <div>Single</div>
+        <div className="single">
+            <div className="content">
+                <img src="https://images3.pixlis.com/background-image-organic-plasma-seamless-tileable-blue-spring-green-22523r.png" alt="" />
+                <div className="user">
+                    <img src="https://images3.pixlis.com/background-image-organic-plasma-seamless-tileable-blue-spring-green-22523r.png" alt="" />
+                    <div className="info">
+                        <span>Jon Doe</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                    <div className="edit">
+                        <Link to='/write?edit=2'>
+                            <img src={Edit} alt="" />
+                        </Link>
+                        <img src={Delete} alt="" />
+                    </div>
+                </div>
+                <h1>title</h1>
+                <p>body</p>
+            </div>
+            <Menu />
+        </div>
     );
 }
 
