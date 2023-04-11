98a3a9af6a1be663c9d40b393a1a42d53c0e4332
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-04-11 16:29:47 -0500

    start register functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:""
+    });
+
+    const handleChange = e => {
+        setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try{
+            const res = await axios.post("/auth/register", inputs);
+            console.log(res);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
     return (
         <div className="auth">
             <h1>Register</h1>
             <form>
-                <input required type="text" placeholder="username" />
-                <input required type="password" placeholder="password" />
-                <input required type="email" placeholder="email" />
-                <button>Register</button>
+                <input required type="text" placeholder="username" name="username" onChange={handleChange}/>
+                <input required type="email" placeholder="email" name="email" onChange={handleChange}/>
+                <input required type="password" placeholder="password" name="password" onChange={handleChange}/>
+                <button onClick={handleSubmit}>Register</button>
                 <p>This is an error</p>
-                <span>Have an account? <Link to="/login">Register</Link></span>
+                <span>Have an account? <Link to="/login">Login</Link></span>
             </form>
         </div>
     );
